3f02c673dc0d3b1397d02f00f04669c83ff5775e
Kansei Kogami <kk7221dev@gmail.com>
2019-01-23 08:28:26 +0900

    add: delete_expired_post method

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,10 @@
 class Post < ApplicationRecord
+
+  def self.delete_expired
+    expiration_min = 30
+    deadline = Time.now - expiration_min*60
+
+    expired_posts = Post.where("created_at < ?", deadline)
+    expired_posts.destroy_all
+  end
 end
