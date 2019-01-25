e3f6adfd53722b2ba1976688a7267df49a218fee
Kansei Kogami <kk7221dev@gmail.com>
2019-01-25 22:56:52 +0900

    add: error handling

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,5 +1,3 @@
-require 'byebug'
-
 class Api::V1::PostsController < ApplicationController
   def index
     @post = Post.all
@@ -7,25 +5,17 @@ class Api::V1::PostsController < ApplicationController
 
   def create
     @post = Post.new(post_params)
-
-    unless @post.save
-      render json: @post.errors, status: :unprocessable_entity
-    end
+    @post.save!
 
     render :show, status: :created
   end
 
   def update
     @post = Post.find(params[:id])
-
     @post.sympathy += 1
-
     @post.save!
 
     render :show, status: :ok
-
-    rescue => error
-      render json: {message: error}, status: :unprocessable_entity
   end
 
   private
